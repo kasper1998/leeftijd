876b2613011c274542c9950ac6bb899c9c3f5241
K. Bosteels <kasperbosteels@hotmail.com>
2020-09-28 16:26:21 +0200

    intial code setup

---
+++ leeftijd berekenen/leeftijd berekenen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace leeftijd_berekenen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+    class welcom
+    {
+        public string bericht()
+        {
+            string welkom = "welcome deze app wordt gebruikt om je leeftijd te berekenen";
+            return welkom;
+        }
+    }
+    class Input
+    {
+
+    }
+    class Output
+    {
+
+    }
+
+}
