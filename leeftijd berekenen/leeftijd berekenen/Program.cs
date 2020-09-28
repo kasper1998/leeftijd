809ee4fcdd5a0579e782acb8b654d794990abc9c
K. Bosteels <kasperbosteels@hotmail.com>
2020-09-28 16:43:33 +0200

    welkom in aparte file

--- leeftijd berekenen/leeftijd berekenen/Program.cs
+++ leeftijd berekenen/leeftijd berekenen/Program.cs
@@ -9,14 +9,7 @@ namespace leeftijd_berekenen
           
         }
     }
-    class welcom
-    {
-        public string bericht()
-        {
-            string welkom = "welcome deze app wordt gebruikt om je leeftijd te berekenen";
-            return welkom;
-        }
-    }
+    
     class Input
     {
 
---
+++ leeftijd berekenen/leeftijd berekenen/welkom.cs
@@ -0,0 +1,8 @@
+class welcom
+    {
+        public string bericht()
+        {
+            string welkom = "welcome deze app wordt gebruikt om je leeftijd te berekenen";
+            return welkom;
+        }
+    }
