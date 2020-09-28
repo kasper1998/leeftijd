2af38d1b1178165688186604086efc67ee3840d7
K. Bosteels <kasperbosteels@hotmail.com>
2020-09-28 17:12:37 +0200

    console write

--- leeftijd berekenen/leeftijd berekenen/welkom.cs
+++ leeftijd berekenen/leeftijd berekenen/welkom.cs
@@ -1,8 +1,9 @@
+using System;
+
 class welcom
     {
         public string bericht()
         {
-            string welkom = "welcome deze app wordt gebruikt om je leeftijd te berekenen. de ingegeven data word voor geen andere doeleinden gebruikt. geef Aub uw geboorte datum in in het volgende formaat DD/MM/YYYY";
-            return welkom;
+            console.write(             "welcome deze app wordt gebruikt om je leeftijd te berekenen. de ingegeven data word voor geen andere doeleinden gebruikt. geef Aub uw geboorte datum in in het volgende formaat DD/MM/YYYY");
         }
     }
