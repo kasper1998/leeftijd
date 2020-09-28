6b230937e3170a25e4cdcdb3a9eac359b8382489
Bart <s110266@ap.be>
2020-09-28 17:15:14 +0200

    Versie 1.0

--- leeftijd berekenen/leeftijd berekenen/welkom.cs
+++ leeftijd berekenen/leeftijd berekenen/welkom.cs
@@ -2,8 +2,8 @@ using System;
 
 class welcom
     {
-        public string bericht()
+        public void bericht()
         {
-            console.write(             "welcome deze app wordt gebruikt om je leeftijd te berekenen. de ingegeven data word voor geen andere doeleinden gebruikt. geef Aub uw geboorte datum in in het volgende formaat DD/MM/YYYY");
+            Console.WriteLine(             "welcome deze app wordt gebruikt om je leeftijd te berekenen. de ingegeven data word voor geen andere doeleinden gebruikt. geef Aub uw geboorte datum in in het volgende formaat DD/MM/YYYY");
         }
     }
