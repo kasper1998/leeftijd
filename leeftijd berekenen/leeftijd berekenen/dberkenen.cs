4a2b329cf1187679cbf49620f5b6cdf83b1f5e90
Kasper <kasperbosteels2@gmail.com>
2020-10-05 16:40:14 +0200

    dagen berekenen

---
+++ leeftijd berekenen/leeftijd berekenen/dberkenen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+
+class dberkenen
+{
+	public int Dberekening(DateTime birthday)
+	{
+		DateTime current = DateTime.Today;
+		DateTime next = new DateTime(birthday.Day, birthday.Month, current.Year);
+		if (current < current)
+			next = next.AddYears(1);
+		int dagen = (next - current).Days;
+		return dagen;
+	}
+
+}
